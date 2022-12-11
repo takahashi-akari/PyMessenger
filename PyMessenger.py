459df34233721c314d691e80c5102ff8f1a0614b
Hayashizaki Masayuki / 林崎優之 <akaritakahashioss@gmail.com>
2022-12-11 11:47:58 +0900

    first commit

--- PyMessenger.py
+++ PyMessenger.py
@@ -1,14 +1,18 @@
 import PySimpleGUI as sg
 import threading
 import time
+import socket
 import socketio
 import uvicorn
 import requests
-import multiprocessing
 from uvicorn import Config, Server
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 from requests.packages.urllib3 import disable_warnings
 disable_warnings(InsecureRequestWarning)
+PORT=8000
+window = None
+# create a Socket.IO Client
+sio_client = socketio.Client(engineio_logger=False, logger=False, ssl_verify=False)
 
 class UvicornServer(threading.Thread):
     def __init__(self, config: Config):
@@ -40,24 +44,42 @@ def disconnect(sid):
 # receive a message from a client
 @sio.event
 def message(sid, data):
+    datetime = time.strftime("%Y-%m-%d %H:%M:%S ", time.localtime())
+    window['-LOG-'].update(datetime + sid + ': ' + data + "\n" + window['-LOG-'].get())
+    window.refresh()
     print('message: ', data)
 
-def createWindow():
+def createWindow(ip):
     sg.theme('DarkAmber')   # Add a touch of color
     # All the stuff inside your window.
-    layout = [  [sg.Text('Enter a message and press send')],
+    layout = [
+                [sg.Text('Welcome to PyMessenger!', size=(30, 1), font=("Helvetica", 25))],
+                [sg.Text('Send IP address')],
+                [sg.Input(key='-IP-')],
+                [sg.Text('Send Port')],
+                [sg.Input(key='-PORT-')],
+                [sg.Text('Enter a message and press send')],
                 [sg.Input(key='-IN-')],
-                [sg.Button('Send'), sg.Button('Exit')] ]
+                [sg.Button('Send')],
+                [sg.Text('Log')],
+                [sg.Multiline(key="-LOG-", size=(88, 20), default_text=ip)],
+                [sg.Button('Exit')]
+            ]
 
     # Create the Window
     return sg.Window('PyMessenger', layout)
 
 if __name__ == '__main__':
-    config = Config(app, host='0.0.0.0', port=8000, log_level="info", ssl_keyfile="server.key", ssl_certfile="server.crt")
+    config = Config(app, host='0.0.0.0', port=PORT, log_level="error", ssl_keyfile="server.key", ssl_certfile="server.crt")
     instance = UvicornServer(config)
     instance.start()
 
-    window = createWindow()
+    host = socket.gethostname()
+    ip = "Waiting for connection..." + "\n"
+    ip = 'Running On: ' + socket.gethostbyname(host) + ":" + str(PORT) +"\n" + ip
+
+    window = createWindow(ip)
+
     while True:
         event, values = window.read()
         if event == sg.WIN_CLOSED or event == 'Exit': # if user closes window or clicks cancel
@@ -66,7 +88,12 @@ if __name__ == '__main__':
             break
 
         if event == 'Send':
+            # connect to the server
+            sio_client.connect('https://' + values['-IP-'] + ':' + values['-PORT-'])
+            sio_client.emit('message', values['-IN-'])
             # print(values[0])
-            sio.emit('message', values[0])
+            #sio.emit('message', values[0])
+            datetime = time.strftime("%Y-%m-%d %H:%M:%S ", time.localtime())
+            window['-LOG-'].update(datetime + 'You: ' + values['-IN-'] + "\n" + window['-LOG-'].get())
             window['-IN-'].update('')
             window.refresh()
--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # PyMessenger
 A simple python script to send messages to your friends on LocalNetwork.
+
+## Requirements
+- Python 3.6 or above
+- Python modules: `requests`, `socketio`, `PySimpleGUI`
+
+## Usage
+
+```bash
+$ openssl genrsa 4096 > server.key
+$ openssl req -new -x509 -nodes -sha256 -days 3650 -key server.key > server.crt
+```
+  
+- Run `python3 PyMessenger.py` on your computer.
+
+## Features
+- Send messages to your friends on LocalNetwork.
+
+## TODO
+- Send files to your friends on LocalNetwork.
+
+## Screenshots
+![screenshot](./screenshot.png)
+
+## License
+MIT License (c) 2022 Takahashi Akari <akaritakahashioss@gmail.com>
